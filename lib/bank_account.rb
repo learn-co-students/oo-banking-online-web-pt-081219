539787b681734912638dd89e27952bbec946e03e
Roberto Medina <robby.medina562@gmail.com>
2019-09-27 08:39:50 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
-
+  @@all = []
+  attr_reader :name
+  attr_accessor :balance, :status
+  def initialize(name)
+    @name= name
+    @status = "open"
+    @balance = 1000
+  end
+  def deposit(amount)
+    @balance += amount
+  end
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  def valid?
+    if @status == "open" && @balance > 0
+    true
+  else false
+  end
+end
+  def close_account
+    @status= "closed"
+  end
 end
