c02d30a47343f7309f0771c8c1c94bf886459667
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-07 03:35:33 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
   
   attr_accessor :sender, :receiver, :amount
-  attr_reader :status
+  attr_accessor :status
   
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -14,36 +14,29 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
   
-  
-   # let(:avi) { BankAccount.new("Avi") }
-   # let(:amanda) { BankAccount.new("Amanda") }
-   # let(:transfer) { Transfer.new(amanda, avi, 50) }
-   # let(:bad_transfer) { Transfer.new(amanda, avi, 4000) }
-
-   # it "can execute a successful transaction between two accounts" do
-   #   transfer.execute_transaction
-   #  expect(amanda.balance).to eq(950)
-   #  expect(avi.balance).to eq(1050)
-   #   expect(transfer.status).to eq("complete")
-  
   def execute_transaction
-    #execute successful transaction between 2 accts
-    #each transfer can only happen once
-    #rejects a transfer if the sender does not have enough funds (does not have a valid account)
-    
     if valid? && sender.balance > amount && self.status == "pending"
       receiver.balance += amount
       sender.balance -= amount
       self.status = "complete"
     else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      reject_transfer
     end
   end
   
   def reverse_transfer
-    #reverse
-    #can only reverse executed transfers
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+  
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
   end
 
 end
